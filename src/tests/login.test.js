b2c0d33abd1c0ac30e68c8d97e5fd3785fc3aae7
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-02-14 10:54:29 -0300

    req-5-refatorado

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -2,6 +2,10 @@ import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
+import Wallet from '../pages/Wallet';
+
+const emailinput = 'email-input';
+const passInput = 'password-input';
 
 describe('login test', () => {
   test('Pagina de login esta na rota / ', () => {
@@ -11,12 +15,12 @@ describe('login test', () => {
   });
   test('existe o input email', () => {
     renderWithRouterAndRedux(<App />);
-    const email = screen.getByTestId('email-input');
+    const email = screen.getByTestId(emailinput);
     expect(email).toBeInTheDocument();
   });
   test('existe o input password', () => {
     renderWithRouterAndRedux(<App />);
-    const senha = screen.getByTestId('password-input');
+    const senha = screen.getByTestId(passInput);
     expect(senha).toBeInTheDocument();
   });
   test('existe um button com nome entrar', () => {
@@ -26,13 +30,49 @@ describe('login test', () => {
   });
   test('se é feito o login e muda a rota para /carteira', () => {
     const { history } = renderWithRouterAndRedux(<App />);
-    const email = screen.getByTestId('email-input');
+    const email = screen.getByTestId(emailinput);
     userEvent.type(email, 'teste@teste.com');
-    const senha = screen.getByTestId('password-input');
+    const senha = screen.getByTestId(passInput);
     userEvent.type(senha, 'leo123');
     const button = screen.getByRole('button', { name: /Entrar/i });
     userEvent.click(button);
     const { pathname } = history.location;
     expect(pathname).toBe('/carteira');
+  });
+});
+describe('wallet test', () => {
+  test('se mostra o email na tela', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const emailRender = screen.getByTestId('email-field');
+    expect(emailRender).toBeInTheDocument();
+  });
+  test('se tem um campo para despesa', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const valor = screen.getByLabelText('Valor:');
+    expect(valor).toBeInTheDocument();
+    userEvent.type(valor, '10');
+    expect(valor.value).toBe('10');
+  });
+  test('se tem um campo para descrição', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const descriçao = screen.getByLabelText('Descrição:');
+    expect(descriçao).toBeInTheDocument();
+    userEvent.type(descriçao, 'teste');
+    expect(descriçao.value).toBe('teste');
+  });
+  test('se tem um campo para Moeda', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const Moeda = screen.getByLabelText('Moeda:');
+    expect(Moeda).toBeInTheDocument();
+  });
+  test('se tem um campo para Método de Pagamento', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const metodo = screen.getByLabelText('Método de Pagamento:');
+    expect(metodo).toBeInTheDocument();
+  });
+  test('se tem um campo para Categoria:', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const categoria = screen.getByLabelText('Categoria:');
+    expect(categoria).toBeInTheDocument();
   });
 });
